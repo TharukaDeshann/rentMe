0eb8b3c59fe1619b027b6031fbd264c23390a515
Tharuka Deshan <deshanght.22@uom.lk>
2025-09-22 20:30:19 +0530

    chore: unify .gitignore for frontend and backend

---
+++ rentMe_backend/src/main/java/com/example/springrentMe/SpringrentMeApplication.java
@@ -0,0 +1,13 @@
+package com.example.springrentMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringrentMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringrentMeApplication.class, args);
+	}
+
+}
---
+++ rentMe_backend/src/test/java/com/example/springrentMe/SpringrentMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springrentMe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringrentMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
